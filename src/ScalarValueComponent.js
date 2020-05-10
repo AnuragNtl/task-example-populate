73ca4ad32c0da012e5cfd7a43f6c9b19bf4c2746
Anurag <anuragshrm70@gmail.com>
2020-05-10 20:37:38 +0530

    Sun May 10 20:37:38 IST 2020

---
+++ src/ScalarValueComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export class NumberRotater extends React.Component {
+    render() {
+        return (
+            <div>
+             
+            </div>
+        );
+    }
+};
